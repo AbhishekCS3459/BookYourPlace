34f9f3bf3fab765428ac7c5c8b4db814c7ce7045
Abhishek Kumar <94506000+AbhishekCS3459@users.noreply.github.com>
2023-08-14 22:01:48 +0530

    Update CurvedNav.jsx

--- client/src/CurvedNav.jsx
+++ client/src/CurvedNav.jsx
@@ -2,20 +2,20 @@
 
 export default function CurvedNav() {
   return (
-    <div className="flex gap-2  border rounded-full py-2 px-4 shadow-lg shadow-gray-300 border-gray-300 cursor-pointer">
-    <div>Anywhere</div>
-    <div className=" border-l border-gray-300"></div>
-    <div>Any Week</div>
-    <div className=" border-l border-gray-300"></div>
-    <div>Add Guest</div>
-    <button className="bg-primary text-white p-1 rounded-full">
+    <div className="flex gap-2  border rounded-full py-2 px-2 sm:py-2 sm:px-4   shadow-lg shadow-gray-300 border-gray-300 cursor-pointer text-[7px] sm:text-lg  text-center h-8 sm:h-auto "  >
+    <div className="my-0.5 sm:my-0 ">Anywhere</div>
+    <div className=" border-l border-gray-300  h-4 sm:h-auto "></div>
+    <div className="my-0.5 sm:my-0">Any Week</div>
+    <div className=" border-l border-gray-300  h-4 sm:h-auto "></div>
+    <div className="my-0.5 sm:my-0">Add Guest</div>
+    <button className="bg-primary text-white p-1  rounded-full sm:rounded-2xl sm:p-2  flex justify-center items-center">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
         strokeWidth={1.5}
         stroke="currentColor"
-        className="w-4 h-4"
+        className="w-3 h-2 sm:w-4 sm:h-4"
       >
         <path
           strokeLinecap="round"
